936712c549a0ca0897f35bce485a926052f20864
Ahmed <oladapoahmed5@gmail.com>
2023-12-01 01:56:52 +0100

    Refactor the code to meet pycodestyle requirements

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,4 +11,8 @@ if last_digit > 5:
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(
+        "Last digit of {:d} is {:d} and is less than 6 and not 0".format(
+            number, last_digit
+        )
+    )
